c2a33a304a641ae5bcfca01149922ce0996140fb
Vlad <hraban@vivec.co.uk>
2018-08-30 22:45:24 +0100

    Add movie model and ability to get/create a new movie

--- movie_list/api/models.py
+++ movie_list/api/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    director = models.CharField(max_length=255, blank=True, null=True)
+    release_year = models.IntegerField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
--- movie_list/api/views.py
+++ movie_list/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import MovieSerializer
+from .models import Movie
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+
+
+    def perform_create(self, serializer):
+        serializer.save()
