9c108c6d1ec08987a1c5820e4db028e3518c33c7
Vlad von Hraban <vlad@vivec.co.uk>
2018-08-30 23:25:48 +0100

    Introduce delete/get resource/update for movies

--- movie_list/api/tests.py
+++ movie_list/api/tests.py
@@ -3,7 +3,6 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from .models import Movie
-from datetime import datetime
 from dateutil import parser
 
 
@@ -37,8 +36,6 @@ class ViewTestCase(TestCase):
         self.assertIn("date_modified", response_body)
 
 
-
-
     def test_validates_input_when_creating_movie(self):
         before_count = Movie.objects.count()
         body = {}
@@ -62,3 +59,44 @@ class ViewTestCase(TestCase):
         stored_movie = Movie.objects.get(id=response_body['id'])
         self.assertEqual(body['director'], stored_movie.director)
 
+    def test_can_retrieve_a_movie(self):
+        movie = Movie.objects.create(name='Manderlay', director='Lars von Trier', release_year=2005)
+        response = self.client.get(
+            reverse('details', kwargs={'pk': movie.id}),
+            format="json")
+        response_body = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_body['id'], movie.id)
+        self.assertEqual(response_body['name'], movie.name)
+        self.assertEqual(response_body['director'], movie.director)
+        self.assertEqual(parser.parse(response_body['date_created']), movie.date_created)
+        self.assertEqual(parser.parse(response_body['date_modified']), movie.date_modified)
+
+
+    def test_can_update_a_movie(self):
+        new_movie = Movie.objects.create(name='Manderley', director='Lars von Trier', release_year=2005)
+        movie_changes = {'name': 'Manderlay'}
+        response = self.client.put(
+            reverse('details', kwargs={'pk': new_movie.id}),
+            data=movie_changes,
+            format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        movie = Movie.objects.get(id=new_movie.id)
+        self.assertEquals(movie.name, movie_changes['name'])
+
+    def test_can_delete_a_movie(self):
+        movie_to_be_deleted = Movie.objects.create(name='Manderley', director='Lars von Trier', release_year=2005)
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': movie_to_be_deleted.id}),
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEquals(Movie.objects.filter(id=movie_to_be_deleted.id).count(), 0)
+
+    def test_handles_movie_not_found(self):
+        response = self.client.get(
+            reverse('details', kwargs={'pk': 999}),
+            format="json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
