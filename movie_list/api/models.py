3686d0b8129d055e59cf28e7c0116d5887620198
Vlad von Hraban <vlad@vivec.co.uk>
2018-10-01 21:02:46 +0100

    Add buckets resource

--- movie_list/api/models.py
+++ movie_list/api/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Bucket(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
 
 class Movie(models.Model):
     name = models.CharField(max_length=255, blank=False, unique=True)
--- movie_list/api/urls.py
+++ movie_list/api/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView
+from .views import MovieCreateView, MovieDetailsView, BucketCreateView, BucketDetailsView
 
 urlpatterns = {
-    url(r'^movies/$', CreateView.as_view(), name="create"),
-    url(r'^movies/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
+    # Movies
+    url(r'^movies/$', MovieCreateView.as_view(), name="movie.create"),
+    url(r'^movies/(?P<pk>[0-9]+)/$', MovieDetailsView.as_view(), name="movie.details"),
+    # Buckets
+    url(r'^buckets/$', BucketCreateView.as_view(), name="bucket.create"),
+    url(r'^buckets/(?P<pk>[0-9]+)/$', BucketDetailsView.as_view(), name="bucket.details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
